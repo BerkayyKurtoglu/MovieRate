885a7a497e6a9ccb72ff73146e7e359afa81c476
Berkay Kurtoglu <berkayberkay300@hotmail.com>
2023-01-29 16:02:38 +0300

    first 1.2

--- app/src/main/java/com/berkaykurtoglu/githubapptrying/MainActivity.kt
+++ app/src/main/java/com/berkaykurtoglu/githubapptrying/MainActivity.kt
@@ -14,6 +14,7 @@ import com.berkaykurtoglu.githubapptrying.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
